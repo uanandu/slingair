815f4bec34785ede6856199079e16833a6ca94cd
Ananda Narayanan Udayakumar <ananduun@icloud.com>
2022-07-26 11:32:59 -0400

    small changes

--- backend/batchImport.js
+++ backend/batchImport.js
@@ -12,22 +12,30 @@ const {flights, reservations} = require("./data");
 
 const batchImport = async () => {
 
-    let flightSeats = [];
+    let flightsList = [];
     
-    flightSeats.push(flights);
+    flightsList.push(
+      {
+        _id: Object.keys(flights).toString(),
+        flight: Object.keys(flights).toString(),
+        seats: Object.values(flights.SA231),
+      }
+    );
     
-
+    // console.log(flightsList)
 
   try {
     const client = new MongoClient(MONGO_URI, options);
 
     await client.connect(); // connect to the client
 
+    console.log("Connected to MongoDB");
+
     const db = client.db("slingair"); // declare the database
     
-    // const result = await db.collection("flightSeats").insertMany(flightSeats); // insert the flight into the database as a new collection
+    const result = await db.collection("flightsList").insertOne(flightsList); // insert the flight info into the database as a new collection
 
-    const result2 = await db.collection("reservations").insertMany(Object.values(reservations)); // insert the seats into the database as a new collection
+    // const result2 = await db.collection("flightsList").insertMany(Object.values(reservations)); // insert the reservation info into the database as a new collection
 
     // console.log("result here", result);
     
